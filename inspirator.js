40c679668d427ab64416e26b65e51c102f7beb58
PixelHUN <59181913+PixelHUN@users.noreply.github.com>
2023-02-09 17:52:36 +0100

    the screenshot update

--- inspirator.js
+++ inspirator.js
@@ -1,29 +1,30 @@
+
+
+let bgCount = 0;
+let bgCols = ["#1a0d16","#1b180e","#112423","#241111","#171124"];
 function changeCol()
 {
-	let bgCols = ["#1a0d16","#1b180e","#112423","#241111","#171124"];
-	//let buttonCols = ["#0f080d","#1b180e","#112423","#241111","#171124"];
-	var col = document.body.style.backgroundColor;
-	do
-	{
-		document.body.style.backgroundColor = bgCols[Math.floor(Math.random() * bgCols.length)];
-	} while(col == document.body.style.backgroundColor);
+	bgCount += 1;
+	if(bgCount >= bgCols.length) bgCount = 0;
+	document.body.style.backgroundColor = bgCols[bgCount];
+	generateImage();
 }
 
 let fontcount = 0;
+let fonts = ["Lato","Alegreya","Great Vibes"];
 
 function changeFont()
 {
-	let fonts = ["Lato","Alegreya","Great Vibes"];
 	//let buttonCols = ["#0f080d","#1b180e","#112423","#241111","#171124"];
 	fontcount += 1;
 	if(fontcount >= fonts.length) fontcount = 0;
 	document.body.style.fontFamily = fonts[fontcount];
+	generateImage();
 }
-
 
 function buttonGenerate()
 {
-	var label = document.getElementById("text");
+	let label = document.getElementById("text");
 	let basis = [];
 	let nouns = [];
 	let verbs = [];
@@ -48,16 +49,16 @@ function buttonGenerate()
 				switch(readingState)
 				{
 					case 1:
-						basis.push(lines[i]);
+						basis.push(lines[i].trim());
 						break;
 					case 2:
-						nouns.push(lines[i]);
+						nouns.push(lines[i].trim());
 						break;
 					case 3:
-						verbs.push(lines[i]);
+						verbs.push(lines[i].trim());
 						break;
 					case 4:
-						adjs.push(lines[i]);
+						adjs.push(lines[i].trim());
 						break;
 				}
 			}
@@ -76,7 +77,7 @@ function buttonGenerate()
 				if(randomInt == 2) theSentence = theSentence.replace("$blank",verbs[Math.floor(Math.random() * verbs.length)].replace(" ",""));
 				if(randomInt == 3) theSentence = theSentence.replace("$blank",adjs[Math.floor(Math.random() * adjs.length)].replace(" ",""));
 			} while(theSentence.includes('$'));
-			
+
 			setTimeout(function() {
 				label.classList.add("changing");
 				setTimeout(function() {
@@ -84,6 +85,7 @@ function buttonGenerate()
 				  label.textContent = label.textContent.charAt(0).toUpperCase() + label.textContent.slice(1);
 				  console.log(theSentence);
 				  label.classList.remove("changing");
+				  generateImage();
 				}, 200);
 			  }, 200);
 
@@ -92,3 +94,40 @@ function buttonGenerate()
 	
 	
 }
+
+function generateImage()
+{
+	let label = document.getElementById("text");
+	let canvas = document.getElementById("saveCanvas");
+	let context = canvas.getContext("2d");
+	context.clearRect(0,0,document.getElementById("saveCanvas").width,document.getElementById("saveCanvas").height);
+	context.font = "32px "+fonts[fontcount];
+	context.textAlign = "center";
+	context.beginPath();
+	context.rect(0,0,canvas.width,canvas.height);
+	context.fillStyle = bgCols[bgCount];
+	context.fill();
+	context.fillStyle = "aliceblue";
+	context.fillText("Inspirator",canvas.width/2,64);
+	wrapText(context, label.textContent, canvas.width/2, canvas.height/2, 450, 40);
+}
+
+function wrapText(context, text, x, y, maxWidth, lineHeight) {
+	var words = text.split(' ');
+	var line = '';
+
+	for(var n = 0; n < words.length; n++) {
+	  var testLine = line + words[n] + ' ';
+	  var metrics = context.measureText(testLine);
+	  var testWidth = metrics.width;
+	  if (testWidth > maxWidth && n > 0) {
+		context.fillText(line, x, y);
+		line = words[n] + ' ';
+		y += lineHeight;
+	  }
+	  else {
+		line = testLine;
+	  }
+	}
+	context.fillText(line, x, y);
+  }
